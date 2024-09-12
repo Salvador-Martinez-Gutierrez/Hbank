b453b45713ce86a8fd9e3cf65463c650b3b748ba
Salva <salva.martinez.gutierrez@gmail.com>
2024-09-13 00:08:57 +0200

    Wallet Connect

--- src/app/context/WalletContext.tsx
+++ src/app/context/WalletContext.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { HWBridgeProvider } from '@buidlerlabs/hashgraph-react-wallets'
 import { HashpackConnector, KabilaConnector } from '@buidlerlabs/hashgraph-react-wallets/connectors'
 import { HederaTestnet } from '@buidlerlabs/hashgraph-react-wallets/chains'
 
 const WalletContext = createContext(null)
 
-const metadata = {
-  name: ' Hbank dApp',
-  description: 'The All-In-One Hubb for Hedera Investors',
-  icons: ['https://pbs.twimg.com/media/GWtnpkjWkAAGrMv?format=png&name=small'],
-  url: window.location.href
+interface Metadata {
+  name: string
+  description: string
+  icons: string[]
+  url: string
 }
 
 export function WalletProvider ({
@@ -18,13 +18,30 @@ export function WalletProvider ({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [metadata, setMetadata] = useState<Metadata | null>(null)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setMetadata({
+        name: 'Hbank dApp',
+        description: 'The All-In-One Hubb for Hedera Investors',
+        icons: ['https://pbs.twimg.com/media/GWtnpkjWkAAGrMv?format=png&name=small'],
+        url: window.location.href
+      })
+    }
+  }, [])
+
+  if (metadata === null) {
+    return null // or a loading indicator
+  }
+
   return (
     <HWBridgeProvider
       metadata={metadata}
       projectId={'eb3e42580d8e325d52e2edd599b9c567'}
       connectors={[HashpackConnector, KabilaConnector]}
       chains={[HederaTestnet]}
-      strategies={[]} // Add this line
+      strategies={[]}
     >
       <WalletContext.Provider value={null}>
         {children}

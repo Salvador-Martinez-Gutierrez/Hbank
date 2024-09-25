d0af0b96c35b8afcc1d2f83e28360fc7fb6d0c02
Salva <salva.martinez.gutierrez@gmail.com>
2024-09-25 15:06:04 +0200

    Made portfolio side menu fixed

--- src/app/address/[accountId]/layout.tsx
+++ src/app/address/[accountId]/layout.tsx
@@ -10,10 +10,10 @@ interface LayoutProps {
 export default function Layout ({ children, params }: LayoutProps) {
   return (
     <div className="bg-neutral-900 flex flex-col md:flex-row w-full">
-      <div className="hidden md:block">
+      <div className="hidden md:block md:fixed md:left-0 md:top-[75px] md:w-64 md:h-[calc(100vh-75px)]">
         <SideNav accountId={params.accountId} />
       </div>
-      <main className="flex-1 min-w-0">{children}</main>
+      <main className="flex-1 min-w-0 md:ml-64 pt-[75px] md:pt-0">{children}</main>
     </div>
   )
 }

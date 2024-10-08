ff0c827d6f2660f8642df5b4d85b4130175f9834
Salva <salva.martinez.gutierrez@gmail.com>
2024-10-08 22:05:16 +0200

    Drop down menu styling

--- src/app/components/ProfileDropDownMenu.tsx
+++ src/app/components/ProfileDropDownMenu.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from 'react'
 import { Button } from '@/app/collections/components/ui/button'
 import {
   DropdownMenu,
@@ -16,9 +17,15 @@ interface ProfileDropDownMenuProps {
 
 export default function ProfileDropDownMenu ({ disconnect }: ProfileDropDownMenuProps) {
   const { data: accountId } = useAccountId()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleItemClick = (action: () => void) => {
+    setIsOpen(false)
+    action()
+  }
 
   return (
-    <DropdownMenu>
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger>
         <Button className='bg-blue-500 hover:bg-blue-500'>
           <span className='pr-1'>
@@ -38,7 +45,10 @@ export default function ProfileDropDownMenu ({ disconnect }: ProfileDropDownMenu
       <DropdownMenuContent className="bg-black w-56 mx-4 lg:mx-8 xl:mx-16 border border-white/10 shadow-[0_0_15px_rgba(255,255,255,0.1)]">
         <DropdownMenuGroup>
           <Link href={`/address/${accountId}`} passHref>
-            <Button className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'>
+            <Button
+              className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'
+              onClick={() => { setIsOpen(false) }}
+            >
               <span className='mr-3'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='#ffffff' viewBox='0 0 256 256'>
                   <path d='M100,116.43a8,8,0,0,0,4-6.93v-72A8,8,0,0,0,93.34,30,104.06,104.06,0,0,0,25.73,147a8,8,0,0,0,4.52,5.81,7.86,7.86,0,0,0,3.35.74,8,8,0,0,0,4-1.07ZM88,49.62v55.26L40.12,132.51C40,131,40,129.48,40,128A88.12,88.12,0,0,1,88,49.62ZM128,24a8,8,0,0,0-8,8v91.82L41.19,169.73a8,8,0,0,0-2.87,11A104,104,0,1,0,128,24Zm0,192a88.47,88.47,0,0,1-71.49-36.68l75.52-44a8,8,0,0,0,4-6.92V40.36A88,88,0,0,1,128,216Z'></path>
@@ -49,7 +59,10 @@ export default function ProfileDropDownMenu ({ disconnect }: ProfileDropDownMenu
           </Link>
 
           <Link href='/wallet-tracker' passHref>
-            <Button className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'>
+            <Button
+              className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'
+              onClick={() => { setIsOpen(false) }}
+            >
               <span className='mr-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                   <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
@@ -60,7 +73,10 @@ export default function ProfileDropDownMenu ({ disconnect }: ProfileDropDownMenu
           </Link>
 
           <Link href='/collections' passHref>
-            <Button className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'>
+            <Button
+              className='bg-black hover:bg-zinc-800 p-4 mb-1 w-full inline-flex cursor-pointer justify-start'
+              onClick={() => { setIsOpen(false) }}
+            >
               <span className='mr-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                   <path d="M160-720v-80h640v80H160Zm0 560v-240h-40v-80l40-200h640l40 200v80h-40v240h-80v-240H560v240H160Zm80-80h240v-160H240v160Zm-38-240h556-556Zm0 0h556l-24-120H226l-24 120Z"/>
@@ -71,7 +87,7 @@ export default function ProfileDropDownMenu ({ disconnect }: ProfileDropDownMenu
           </Link>
 
           <Button
-            onClick={disconnect}
+            onClick={() => { handleItemClick(disconnect) }}
             className='bg-black hover:bg-zinc-800 p-4 w-full inline-flex cursor-pointer justify-start'
           >
             <span className='mr-3'>

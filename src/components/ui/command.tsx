d87cd71bf0a6ee47a07e2f4d2a4ded1ea9ed1059
Salva <salva.martinez.gutierrez@gmail.com>
2024-11-13 14:26:02 +0100

    Fixed search modal styles

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -29,7 +29,7 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg [&_button]:text-white">
+      <DialogContent className="overflow-hidden p-0 shadow-lg w-[calc(100vw-32px)] sm:w-full max-w-[90vw] sm:max-w-[80vw] md:max-w-[600px] [&_button]:text-white rounded-lg">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>

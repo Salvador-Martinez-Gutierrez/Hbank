3283792e801ae228aa8a521799d65c72de55128a
Salva <salva.martinez.gutierrez@gmail.com>
2024-11-13 14:10:47 +0100

    New logo

--- src/app/components/HomeButton.tsx
+++ src/app/components/HomeButton.tsx
@@ -5,7 +5,7 @@ export default function HomeButton () {
   return (
     <Link href="/" passHref>
         <Image
-          src="/logo.png"
+          src="/Logo.png"
           alt="logo"
           width={137}
           height={50}

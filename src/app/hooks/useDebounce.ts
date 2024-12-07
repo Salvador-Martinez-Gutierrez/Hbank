fd905b9b68d269507639f7b456ad1d8f8070dc57
Salva <salva.martinez.gutierrez@gmail.com>
2024-12-08 08:37:38 +1100

    Increased debounce time on search bar to 700

--- src/app/hooks/useDebounce.ts
+++ src/app/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-export default function useDebounce<T> (value: T, delay = 500) {
+export default function useDebounce<T> (value: T, delay = 700) {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
